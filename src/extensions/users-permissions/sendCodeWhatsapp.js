626906fef032a7e35c93af77492a80c34890afd4
S-Kiev <vezequiel58@gmail.com>
2023-10-29 21:22:04 -0300

    Incorporando controladores personalizados para enviar la recuperacion de contraseña por whatsapp

---
+++ src/extensions/users-permissions/sendCodeWhatsapp.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+
+async function sendCodeWhatsApp (code, number) {
+
+    let data = JSON.stringify({
+        "whatsappBotKey": "EsteticaNatural",
+        "code": code,
+        "number": number
+      });
+     
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://whatsapp-bot-node-production.up.railway.app/sendCode',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+    
+    
+      try {
+        axios.request(config).then((res)=>{
+            console.log('Bien');
+            return res
+        })
+      } catch(error) {
+        return error
+      }  
+}
+
+module.exports = {
+    sendCodeWhatsApp
+}
--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,17 +1,13 @@
-let ultimateCode = '';
-let validate = 0;
-let user;
 
+const bcrypt = require('bcrypt');
 const { generateCode } = require('./generateCode');
+const { sendCodeWhatsApp } = require('./sendCodeWhatsapp');
 
 module.exports = (plugin) => {
 
   plugin.controllers.user.sendCode = async (ctx) => {
 
-    //Podria buscar y validar el numero de telefono cuando tenga la tabla infoUsuario
-    if ( !ctx.request.body.number ) {
-
-            if ( !ctx.request.body.username || !ctx.request.body.email ) {
+            if ( ctx.request.body.username || ctx.request.body.email ) {
                 const user = await strapi.query('plugin::users-permissions.user').findOne({
                     where: {
                         $or: [
@@ -19,61 +15,153 @@ module.exports = (plugin) => {
                             username: { $eqi : ctx.request.body.username },
                         },
                         {
-                            createdAt: { $eqi : ctx.request.body.email },
+                            email: { $eqi : ctx.request.body.email },
                         },
                         ],
                     },
-                });
+                }).then( async (res)=>{
 
-                //Aqui buscar el numero de Usuario en la tabla infoUsuario
 
-                if (user == undefined || user == null) {
+                    if ( res.confirmed && !res.blocked ) {
+
+                        const userId = res.id;
+
+                        await strapi.db.query('api::informacion-usuario.informacion-usuario').findOne({
+                            where : {
+                                users_permissions_user : res.id,
+                            }
+                        }).then( async (res) =>{
+
+                            const code = generateCode();
+                            const validSince = new Date().getTime();
+                            const validUntil = validSince + 300000;
+
+                            const number = res.numero;
+
+                            const infoCode = {
+                                code : code,
+                                informacion_usuario : res.id,
+                                validSince : validSince,
+                                validUntil : validUntil,
+                            }
+
+
+                            await strapi.db.query('api::info-user.info-user').create({data : infoCode})
+                            .then((res)=>{
+                                try {
+                                    sendCodeWhatsApp(infoCode.code, number);
+                                    ctx.response.status = 200;
+                                    ctx.response.body = {
+                                        message: `Operacion ejecutada correctamente`,
+                                        userId : userId,
+                                        code : code,
+                                        number: number
+                                    };
+                                } catch (error) {
+                                    console.log(error);
+                                    ctx.response.status = 401;
+                                    ctx.body = {
+                                        message: `No se pudo enviar el código por WhatsApp`,
+                                    };
+                                }
+                            }).catch((error)=>{
+                                console.log(error);
+                                ctx.response.status = 401;
+                                ctx.body = {
+                                    message: `No se pudo crear registro de recuperacion de contraseña`,
+                                };
+                            });
+
+
+                        }).catch((error) =>{
+                            console.log(error);
+                            ctx.response.status = 401;
+                            ctx.body = {
+                                message: `No se encontro un número de telefono asociado a este usuario`,
+                            };
+                        })
+                    } else {
+                        ctx.response.status = 401;
+                        ctx.response.body = {
+                            message: `Usuario bloquedo o sin confirmar`,
+                        };
+                    }
+
+                }).catch ((error)=>{
+                    console.log(error);
                     ctx.response.status = 401;
-                    ctx.response.body = {
-                    message: `Usuario incorrecto`
+                    ctx.body = {
+                        message: `Usuario incorrecto`,
                     };
-                } else {
-                    const code = generateCode();
-
-                    //Aqui si ya tengo el usuario y codigo guardar esos datos en una tabla junto con el manejo del tiempo para validez
-                    //validate = new Date().getTime() + 60 * 60 * 1000;
-    
-                    //Aqui enviar el codigo por Whatsapp
-
-                    ctx.response.status = 200;
-                    ctx.response.body = {
-                        message: `Operacion ejecutada correctamente`,
-                        code : code,
-                        user: user.id
-                    };
-
-
-
-                }
-
-        } else {
-            ctx.response.status = 401;
-            ctx.body = {
-            message: `Se requiere numero de telefono`
-            };
-        }
-    }
-}
-plugin.controllers.user.validateCode = async (ctx) => {
-
-    if ( !ctx.request.body.code || !ctx.request.body.newPassword) {
-
-        //Con el codigo busco en la tabla (idUser, code, validSince, validUntil) el id del usuario
-        //enviar el id de Usuario y la new Password para que Frontend use el update
-        ctx.response.status = 200;
-        ctx.response.body = {
-            message: `Operacion ejecutada correctamente`
-        };
+                });
 
     } else {
         ctx.response.status = 401;
         ctx.body = {
-        message: `Se requiere un código y/o contraseña`
+        message: `Se requiere un numero, username, o email`
+        };
+    }
+}
+
+
+
+plugin.controllers.user.changePasswordByWhatsapp = async (ctx) => {
+
+    console.log(ctx.request.body);
+    if ( ctx.request.body || ctx.request.body.code || ctx.request.body.newPassword || ctx.request.body.id ){
+        await strapi.db.query('api::info-user.info-user').findOne({
+            where: { code: ctx.request.body.code }
+        }).then( async (res)=>{
+
+            const moment = new Date();
+            const validSince = new Date(res.validSince);
+            const validUntil = new Date(res.validUntil);
+
+            if(moment > validSince && moment < validUntil){
+                const password = bcrypt.hashSync(ctx.request.body.newPassword , 10);
+
+                await strapi.query('plugin::users-permissions.user').update({
+                    where: { id: ctx.request.body.id },
+                    data: { password },
+                }).then((res)=>{
+       
+                    const jwtToken = strapi.plugins['users-permissions'].services.jwt.issue({
+                        id: ctx.request.body.id
+                    });
+                   
+                    ctx.response.status = 200;
+                    ctx.response.body = ({
+                        jwtToken,
+                        user: res,
+                    });
+    
+                }).catch ((error)=>{
+                    console.log(error);
+                    ctx.response.status = 401;
+                    ctx.body = {
+                        message: `No se pudo actualizar al Usuario`,
+                    };
+                });
+            } else {
+                ctx.response.status = 401;
+                ctx.body = {
+                    message: `Su código ha expirado, vuelva a solicitar otro`,
+                };
+            }
+
+
+
+
+        }).catch((error)=>{
+            ctx.response.status = 401;
+            ctx.body = {
+            message: `código incorrecto`
+            };
+        })
+    } else {
+        ctx.response.status = 401;
+        ctx.body = {
+        message: `Solicitud mal formulada`
         };
     }
 }
@@ -91,9 +179,9 @@ plugin.controllers.user.validateCode = async (ctx) => {
         }
       },
       {
-        method: "POST",
-        path: "/user/validateCode",
-        handler: "user.validateCode",
+        method: "PUT",
+        path: "/user/changePasswordByWhatsapp",
+        handler: "user.changePasswordByWhatsapp",
         config: {
           prefix: "",
           policies: []
